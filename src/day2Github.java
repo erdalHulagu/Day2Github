9872564ff461071ff063df7f3f8b50a93288f101
Erdal Hulagu <erdalhulahu@gmail.com>
2022-08-15 19:40:52 +0100

    V3

--- src/day2Github.java
+++ src/day2Github.java
@@ -4,7 +4,7 @@ public class day2Github {
     icindeki dosyalari iliskilendirmek icin kullanilir
     2- git add .  --> Working space'den (yani yerelden dosyalarimi staging area'ya
     (yani commitlemek icin beklenen yer)  gonderir
-    3- git status --> Working space'deki
+    3- git status --> Working space'deki staging areadaki durumu gosterir
     4- git commit -m "mesaj"  --> Staging areadan commit stora dosyyalarimi gondermek icin kullanilir
     (commit yani versiyon yani surum olusturulmus olur).
     5- git push -->uzak repo'ya (yani remot-gitHub) gondermek icin kullandigimiz kod
@@ -12,8 +12,8 @@ public class day2Github {
                 git remote add origin -adress
                 git push -u origin master
      Not: Yukardaki iki komutu tek seferde kullandiktan sonra, diger ikinci commit'lerim icin
-
      sadece git push kullanirim.
+     Not: git log --oneline --> Commitlerdeki versiyonlardaki durumu gosterir
      */
 
 }
